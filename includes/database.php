8780f6411df61ed254483dc450e3e79cdf1a17dd
Leonel Silvera <leonelsilvera9@gmail.com>
2023-10-16 00:26:24 -0300

    Nuevo

--- includes/database.php
+++ includes/database.php
@@ -1,7 +1,6 @@
 <?php
 
 $db = mysqli_connect($_ENV["DB_HOST"], $_ENV["DB_USER"], $_ENV["DB_PASS"], $_ENV["DB_NAME"]);
-// $db = mysqli_connect("localhost", "root", "root", "appsalon_mvc");
 
 $db->set_charset("utf8");
 
